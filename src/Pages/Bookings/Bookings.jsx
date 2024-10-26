783132634c41477a4be6330be1c298d80e2983c4
AminulIslamorange <aminulislamorange@gmail.com>
2024-10-26 19:05:55 +0600

    crud oparetion complite

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -13,28 +13,67 @@ const Bookings = () => {
         .then(data=>{
             setBooking(data);
         })
-    },[])
+    },[url])
+    const handleDelete=id=>{
+        const proceed=confirm('Are you sure want to delete?')
+        if(proceed){
+            fetch(`http://localhost:5000/bookings/${id}`,{
+                method:'DELETE'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+               if(data.deletedCount > 0){
+                alert('Deleted Successfully');
+                const remaining=bookings.filter(booking=>booking._id !==id);
+                setBooking(remaining)
+               }
+               
+            })
+
+        }
+    }
+    const handleConfirm=(id)=>{
+      fetch(`http://localhost:5000/bookings/${id}`,{
+        method:'PATCH',
+        headers:{
+          'content-type':'application/json'
+        },
+        body:JSON.stringify({status:'confirm'})
+      })
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data);
+        if(data.modifiedCount> 0){
+          alert('updated successfully')
+          const remaining=bookings.filter(booking=>booking._id !==id);
+          const updated=bookings.find(booking=>booking._id ===id);
+          updated.status='confirm'
+          const newBooking=[updated,...remaining];
+          setBooking(newBooking)
+        }
+      })
+
+    }
     return (
-        <div>
-            <h5>Your Bookings:{bookings.length}</h5>
+        <div className="my-12">
+            <h5 className="text-3xl text-center">Your Bookings:{bookings.length}</h5>
             <div className="overflow-x-auto">
   <table className="table">
     {/* head */}
     <thead>
       <tr>
         <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
+          Delete
         </th>
         <th>Name</th>
         <th>Email</th>
         <th>Service Name</th>
         <th>Amount</th>
+        <th>Action</th>
       </tr>
     </thead>
     <tbody>
-    {bookings.map(booking=><BookingsRow key={booking._id} booking={booking}></BookingsRow>)}
+    {bookings.map(booking=><BookingsRow key={booking._id} booking={booking} handleDelete={handleDelete} handleConfirm={handleConfirm}></BookingsRow>)}
      
      
       
--- src/Pages/Bookings/BookingsRow.jsx
+++ src/Pages/Bookings/BookingsRow.jsx
@@ -1,40 +1,57 @@
 
 
-const BookingsRow = ({booking}) => {
-    const {customerName,email,service,date,amount,img}=booking;
+const BookingsRow = ({ booking,handleDelete,handleConfirm }) => {
+    const { customerName,_id, email, service, date, amount, img,status } = booking;
+    
+
+
+    
     return (
         <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle h-12 w-12">
-               {img ? <img
-                  src={img}
-                  alt="Avatar Tailwind CSS Component" /> :<div className="avatar">
-                  
-                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                  </div>}
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">{customerName}</div>
-              <div className="text-sm opacity-50">{date}</div>
-            </div>
-          </div>
-        </td>
-        
-        <td>{email}</td>
-        <td>{service}</td>
-        <td>{amount}</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">Details</button>
-        </th>
-      </tr>
+            <th>
+                <button onClick={()=>handleDelete(_id)} className="btn btn-circle btn-outline btn-warning btn-sm">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </th>
+            <td>
+                <div className="flex items-center gap-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                            {img ? <img
+                                src={img}
+                                alt="Avatar Tailwind CSS Component" /> : <div className="avatar">
+
+                                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>}
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{customerName}</div>
+                        <div className="text-sm opacity-50">{date}</div>
+                    </div>
+                </div>
+            </td>
+
+            <td>{email}</td>
+            <td>{service}</td>
+            <td>{amount}</td>
+            <th>
+               { status==='confirm'?<span className="text-yellow-700">APPROVED</span>:
+                <button onClick={()=>handleConfirm(_id)} className="text-yellow-500">Confirm</button>
+                }
+            </th>
+        </tr>
     );
 };
 
